1af0e3c1b47af605b15a45aa92e9b5bddad393af
subburaj04 <psgs4244@gmail.com>
2020-05-25 18:43:24 +0530

    send file to moni

--- saving.py
+++ saving.py
@@ -1,12 +1,17 @@
 
 def showchoice():
-
    print("1.Expense")
    print("2.Income")
    print("3.Statement")
    print("4.Exit")
    usr_choice=int(input("Enter the choice :"))
    return usr_choice
+   
+def opt(choice,balance,transaction_no,balance_amt):
+   if choice==1:expense(balance,transaction_no,transaction,balance_amt)
+   if choice==2:income(balance,transaction_no,transaction,balance_amt)
+   if choice==3:statement(transaction,balance_amt)
+   if choice==4:usr_exit()
 
 def expense(balance,transaction_no,transaction,balance_amt):
    reason=input("Enter the reason : ")
@@ -16,8 +21,6 @@ def expense(balance,transaction_no,transaction,balance_amt):
    transaction.append(exp_row)
    return transaction,balance_amt
 	
-	
-
 def income(balance,transaction_no,transaction,balance_amt):
    reason=input("Enter the reason :  ")
    income_amt=int(input("Enter the amount :"))
@@ -33,14 +36,6 @@ def statement(transaction,balance_amt):
 def usr_exit():
    print("----exit---")
 
-def opt(choice,balance,transaction_no,balance_amt):
-   if choice==1:expense(balance,transaction_no,transaction,balance_amt)
-   if choice==2:income(balance,transaction_no,transaction,balance_amt)
-   if choice==3:statement(transaction,balance_amt)
-   if choice==4:usr_exit()
-	
-
-    
 if __name__=="__main__":
    transaction_no=0
    transaction=[]
---
+++ saving_class.py
@@ -0,0 +1,17 @@
+class saving_account():
+    def __init__ (self,first,last,initial):
+        self.first_name = first
+        self.last_name = last
+        self.initial_amount = initial
+	
+    def showchoice():
+		print("1.Expense")		
+		print("2.Income")
+		print("3.Statement")
+		print("4.Exit")
+		usr_choice=int(input("Enter the choice :"))
+		return usr_choice
+
+if __name__=="__main__":
+	test=saving_account("firstname","lastname",5000)
+	print(test.first_name)
